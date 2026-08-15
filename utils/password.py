7f5f0d1e6e0cc85bfceafc8c533ca504de650d22
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-08-15 10:47:49 +0330

    Hash password and Verify password

---
+++ utils/password.py
@@ -0,0 +1,38 @@
+# Libs
+from pwdlib import PasswordHash  # Password
+import re  # Regex
+
+# Hash instance
+password_hash = PasswordHash.recommended()
+
+
+# Hash password
+def hash_password(password: str) -> str:
+    return password_hash.hash(password)
+
+
+# Verify password
+def verify_password(password: str, hashed: str) -> bool:
+    return password_hash.verify(password, hashed)
+
+
+def validate_password_strength(password: str) -> bool:
+    if not isinstance(password, str):
+        return False
+
+    if len(password) < 12:
+        return False
+
+    if not re.search(r"[a-z]", password):
+        return False
+
+    if not re.search(r"[A-Z]", password):
+        return False
+
+    if not re.search(r"\d", password):
+        return False
+
+    if not re.search(r"[^A-Za-z0-9]", password):
+        return False
+
+    return True
