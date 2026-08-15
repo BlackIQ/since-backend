b69254cd252e19e867dd801b37840ee969d84f33
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-08-15 11:07:17 +0330

    Cors

--- main.py
+++ main.py
@@ -33,6 +33,8 @@ app.add_middleware(
     allow_origins=[
         "https://since.amirhossein.info",
         "http://localhost:3000",
+        "http://127.0.0.1:8000",
+        "http://localhost:8000",
         "http://127.0.0.1:3000",
     ],
     allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
