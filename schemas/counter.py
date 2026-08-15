a5cdb80e8498b3f1b98da61ddc80088582a42b50
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-08-15 09:31:19 +0330

    Add: Counter schema

---
+++ schemas/counter.py
@@ -0,0 +1,34 @@
+# Libs
+from datetime import datetime  # Datetime
+import uuid  # UUID
+
+# Application
+from base.schema import BaseSchema  # Base
+from enums.visibility import VisibilityType  # Visibility Enum
+from enums.status import StatusType  # Status Enum
+
+
+# Create Counter Schema
+class CounterCreate(BaseSchema):
+    title: str
+    description: str
+    visibility: VisibilityType
+    status: StatusType
+
+    user_id: uuid.UUID
+
+
+# Update Counter Schema
+class CounterUpdate(BaseSchema):
+    title: str | None = None
+    description: str | None = None
+    visibility: VisibilityType
+    status: StatusType
+
+
+# Read Counter Schema
+class CounterRead(CounterCreate):
+    id: uuid.UUID
+
+    created_at: datetime
+    updated_at: datetime
